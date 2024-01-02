c67613d4b14e8afb537af2bdc0d5873280277dc4
Andreas Sünder <asuender@student.tgm.ac.at>
2024-01-02 21:16:28 +0100

    Add fine-tuning shell script

---
+++ training/finetune.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# == USER SETTINGS == #
+HF_TOKEN=
+WANDB_TOKEN=
+AUTOSTOP=0
+
+# check if HF_TOKEN is set
+if [ -z "$HF_TOKEN_" ] || [ -z "$WANDB_TOKEN" ];
+then
+    echo "Gotcha! Please set HF_TOKEN and WANDB_TOKEN before continuing."
+    exit 1
+fi
+export HF_TOKEN=$HF_TOKEN
+
+# login to wandb
+echo "Logging in to wandb..."
+wandb login $WANDB_TOKEN
+if [ $? -ne 0 ]; then
+    echo "Failed to login to wandb."
+    exit 1
+fi
+
+echo "Everything is set up. Starting training..."
+python ./scripts/sft.py $@
+
+# stop pod when training is finished
+if [ $AUTOSTOP -eq 1 ]; then
+    echo "Shutting down..."
+    runpodctl stop pod $RUNPOD_POD_ID
+fi
