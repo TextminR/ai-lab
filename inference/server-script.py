6f5a9b8854473e880304b99a889fe3550fc87c18
bkissinger <benjamin@kissinger.at>
2024-01-13 22:26:46 +0100

    server-script for prototype

---
+++ inference/server-script.py
@@ -0,0 +1,14 @@
+# PREREQUESITES:
+
+# model_id = 'textminr/ner-multilingual-bert'
+# from transformers import pipeline
+# classifier = pipeline(
+#   'ner',
+#   model=model_id,
+#   aggregation_strategy='simple'
+# )
+
+def endpoint(list):
+    #tbd
+    return 0
+    
